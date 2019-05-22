78edca9eba46acb7c533f3ba058f9846c03383f7
25sprout-alan <alan@25sprout.com>
2019-05-22 11:34:39 +0800

    chore: Add storybook 5 and babel-plugin-module-resolver

---
+++ src/components/atoms/Dropdown/__tests__/Dropdown.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Dropdown from 'components/atoms/Dropdown';
+
+storiesOf('Commons|atoms/Dropdown', module)
+    .add('default', () => <Dropdown />); 
