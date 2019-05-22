56e6d258cfadb80d453fad9dfdac9a2b85fbdfaa
25sprout-alan <alan@25sprout.com>
2019-05-22 11:59:37 +0800

    chore: Add storybook 5

---
+++ .storybook/config.js
@@ -0,0 +1,19 @@
+import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
+
+// import 'normalize.css';
+// import 'global.css';
+// import 'babel-polyfill';
+
+setOptions({
+	hierarchySeparator: /\/|\./,
+	hierarchyRootSeparator: /\|/,
+});
+
+function loadStories() {
+  // automatically import all files ending in *.stories.js
+  const req = require.context('../src/components/', true, /\.stories\.js$/);
+  req.keys().forEach(req);
+}
+
+configure(loadStories, module);
