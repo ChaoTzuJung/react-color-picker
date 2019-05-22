cbc6a3ba1f920a0ce1fbbb52a1e50b49a25d4968
25sprout-alan <alan@25sprout.com>
2019-05-23 01:42:21 +0800

    refactor: Refacotr data and ui code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Palette from './components/Palette';
-import seedColors from '../models/seedColors';
+import Palette from 'components/molecules/Palette';
+import colorsConfig from 'utils/colorsConfig';
 
 const App = () => (
     <div>
-      <Palette palette={seedColors[4]} />
+      {/* 有技巧的傳 props */} 
+      <Palette {...colorsConfig[4]}/>
     </div>
 );
 
