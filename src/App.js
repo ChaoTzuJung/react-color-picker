149ade4474cbac1591286d8e6223a94a9c7b051b
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2019-06-02 14:41:06 +0800

    fix: Init platte route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from 'components/molecules/Palette';
 import colorsConfig from 'utils/colorsConfig';
 import generatePalette from 'utils/colorHelper';
 
-const App = () => (
-  <div>
-    <Palette palette={generatePalette(colorsConfig[4])} />
-  </div>
-)
+
+class App extends Component {
+  findPalette = id => {
+    const filterPalette = colorsConfig.find(palette => palette.id === id)
+    return filterPalette;
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>palette list go here!</h1>} />
+        <Route
+          exact path="/palette/:id"
+          render={props => <Palette palette={generatePalette(this.findPalette(props.match.params.id))} />}
+        />
+      </Switch>
+    )
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 // A modern alternative to CSS resets
 import 'normalize.css';
@@ -10,7 +11,12 @@ import './global.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
