bd205718a38e0cc39549bf74808b9a27294af162
25sprout-alan <alan@25sprout.com>
2019-06-11 12:50:45 +0800

    fix: Styled draggable color box

--- src/components/atoms/DraggableColorBox/index.js
+++ src/components/atoms/DraggableColorBox/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/styles';
 
+import DeleteIcon from '@material-ui/icons/Delete';
+
 const styles = {
     root: {
         display: 'inline-block',
@@ -11,13 +13,37 @@ const styles = {
         margin: '0 auto',
         marginBottom: '-3.5px',
         cursor: 'pointer',
+    },
+    boxContent : {
+        display: 'flex',
+        justifyContent: 'space-between',
+        position: 'absolute',
+        width: '100%',
+        left: '0',
+        bottom: '0',
+        padding: '10px',
+        color: 'rgba(0,0,0,0.5)',
+        letterSpacing: '1px',
+        textTransform: 'uppercase',
+        fontSize: '12px',
+        // marginBottom: '-3.5px',
+        "&:hover svg": {
+            color: 'white',
+            transform: 'scale(1.5)',
+        }
+    },
+    deleteIcon: {
+        transition: 'all 0.3s ease-in-out'
     }
 };
 
 const DraggableColorBox = ({ color, name, classes }) => {
     return (
         <div className={classes.root} style={{ backgroundColor: color }}>
-            {name}
+            <div className={classes.boxContent}>
+                <span>{name}</span>
+                <DeleteIcon className={classes.deleteIcon} />
+            </div>
         </div>
     )
 }
