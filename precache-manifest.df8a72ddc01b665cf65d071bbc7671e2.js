3ddf44bf080993b81a43bc2a5236c759bea82e48
25sprout-alan <alan@25sprout.com>
2019-08-04 22:46:53 +0800

    Updates

---
+++ precache-manifest.df8a72ddc01b665cf65d071bbc7671e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df84ba1254ecc008bad71d09541d36b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a449df38a242cd504059",
+    "url": "/static/css/2.f6c124bf.chunk.css"
+  },
+  {
+    "revision": "01c9b8349e1573d1c73c",
+    "url": "/static/css/main.62bbf422.chunk.css"
+  },
+  {
+    "revision": "a449df38a242cd504059",
+    "url": "/static/js/2.348a3d5d.chunk.js"
+  },
+  {
+    "revision": "01c9b8349e1573d1c73c",
+    "url": "/static/js/main.b900ecee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f16e837103683268cec6fab2c76935f5",
+    "url": "/static/media/background.f16e8371.svg"
+  }
+]);
